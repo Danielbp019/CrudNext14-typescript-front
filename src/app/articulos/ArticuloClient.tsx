aa1407b5bd257098348f0f30d7c640c7ff8e13fb
Daniel Bautista P. <76835848+Danielbp019@users.noreply.github.com>
2024-04-15 22:51:16 -0500

    Update ArticuloClient.tsx
    Modal para el formulario con todos los botones listos. Los campos se limpian en crear y cerrar modal.

--- src/app/articulos/ArticuloClient.tsx
+++ src/app/articulos/ArticuloClient.tsx
@@ -86,6 +86,14 @@ export default function ArticuloClient() {
         }
     };
 
+    const limpiarCampos = () => {
+        setTitulo('');
+        setCuerpo('');
+        setAutor('');
+        setIsEditing(false);
+        setEditingId(null);
+    }
+
     const startEdit = (articulo: Articulo) => {
         setEditingId(articulo.id);
         setTitulo(articulo.titulo);
@@ -96,58 +104,78 @@ export default function ArticuloClient() {
 
     return (
         <div>
-            <form onSubmit={isEditing ? handleUpdate : handleCreate}>
-                <div className="mb-3">
-                    <label htmlFor="formTitulo" className="form-label">Título</label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        id="formTitulo"
-                        placeholder="Ingrese el título"
-                        value={titulo}
-                        onChange={(e) => setTitulo(e.target.value)}
-                        minLength={5}
-                        maxLength={200}
-                    />
+
+            <div className="modal fade" id="crearyactModal" tabIndex={-1} aria-labelledby="crearyactModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h1 className="modal-title fs-5" id="crearyactModalLabel">{isEditing ? 'Actualizar articulo' : 'Crear articulo'}</h1>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <form onSubmit={isEditing ? handleUpdate : handleCreate}>
+                                <div className="mb-3">
+                                    <label htmlFor="formTitulo" className="form-label">Título</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="formTitulo"
+                                        placeholder="Ingrese el título"
+                                        value={titulo}
+                                        onChange={(e) => setTitulo(e.target.value)}
+                                        minLength={5}
+                                        maxLength={200}
+                                    />
+                                </div>
+
+                                <div className="mb-3">
+                                    <label htmlFor="formCuerpo" className="form-label">Cuerpo</label>
+                                    <textarea
+                                        className="form-control"
+                                        id="formCuerpo"
+                                        rows={3}
+                                        placeholder="Ingrese el cuerpo"
+                                        value={cuerpo}
+                                        onChange={(e) => setCuerpo(e.target.value)}
+                                        minLength={5}
+                                        maxLength={200}
+                                    />
+                                </div>
+
+                                <div className="mb-3">
+                                    <label htmlFor="formAutor" className="form-label">Autor</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="formAutor"
+                                        placeholder="Ingrese el autor"
+                                        value={autor}
+                                        onChange={(e) => setAutor(e.target.value)}
+                                        minLength={5}
+                                        maxLength={200}
+                                    />
+                                </div>
+                                <br />
+
+                                <div className="modal-footer">
+                                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={() => limpiarCampos()}>Cerrar</button>
+                                    <button type="submit" className="btn btn-primary" data-bs-dismiss="modal">{isEditing ? 'Actualizar' : 'Crear'}</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
+            </div>
 
-                <div className="mb-3">
-                    <label htmlFor="formCuerpo" className="form-label">Cuerpo</label>
-                    <textarea
-                        className="form-control"
-                        id="formCuerpo"
-                        rows={3}
-                        placeholder="Ingrese el cuerpo"
-                        value={cuerpo}
-                        onChange={(e) => setCuerpo(e.target.value)}
-                        minLength={5}
-                        maxLength={200}
-                    />
-                </div>
-
-                <div className="mb-3">
-                    <label htmlFor="formAutor" className="form-label">Autor</label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        id="formAutor"
-                        placeholder="Ingrese el autor"
-                        value={autor}
-                        onChange={(e) => setAutor(e.target.value)}
-                        minLength={5}
-                        maxLength={200}
-                    />
-                </div>
-
-                <button type="submit" className="btn btn-primary">
-                    {isEditing ? 'Actualizar' : 'Crear'}
-                </button>
-            </form>
-
+            <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#crearyactModal" onClick={() => limpiarCampos()}>
+                Crear articulo
+            </button>
+            <br />
+            <hr />
             <div style={{ display: 'flex', justifyContent: 'center' }}>
-                <table className="table table-bordered">
+                <table className="table table-bordered table-striped table-hover">
                     <thead>
-                        <tr>
+                        <tr style={{ textAlign: "center" }}>
                             <th scope="col">Numero</th>
                             <th scope="col">Título</th>
                             <th scope="col">Cuerpo</th>
@@ -163,9 +191,10 @@ export default function ArticuloClient() {
                                 <td>{articulo.cuerpo}</td>
                                 <td>{articulo.autor}</td>
                                 <td>
-                                    <button type="button" className="btn btn-primary" onClick={() => startEdit(articulo)}>
+                                    <button type="button" className="btn btn-primary" onClick={() => startEdit(articulo)} data-bs-toggle="modal" data-bs-target="#crearyactModal">
                                         Editar
                                     </button>
+                                    {" "}
                                     <button type="button" className="btn btn-danger" onClick={() => handleDelete(articulo.id)}>
                                         Eliminar
                                     </button>
@@ -175,6 +204,7 @@ export default function ArticuloClient() {
                     </tbody>
                 </table>
             </div>
+
         </div>
     );
 }
